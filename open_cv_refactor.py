f398508ad011b5b8b4a17caf9c4990b1e9260420
caua <caualimamarques@gmail.com>
2024-05-18 02:39:38 -0300

    Numba Adicionado, erros corrigidos

--- open_cv_refactor.py
+++ open_cv_refactor.py
@@ -1,8 +1,7 @@
 #VIDEO TO ASCII V2
 import cv2 as cv
 import time
-import numpy as np
-from numba import njit,jit
+from numba import njit
 
 print("Você deseja renderizar um vídeo, ou ler um video já renderizado?")
 choice = input("1 - Renderizar video\n2 - Ler video renderizado\n")
@@ -56,55 +55,48 @@ while True:
         print("Não foi encontrado o arquivo informado! Certifique-se de que seu Arquivo é do tipo MP4")
 
 cap = cv.VideoCapture(f"{video_file_name}.mp4") 
-#a largura do meu terminal é 237 caracteres.
-#a altura é 60 caracteres
+
 print("Renderizando...")
 
 @njit
 def processar_pixels(frame_array):
     frame_processado = []
     for height in range(60):
-
         linha_de_pixels = []
         for width in range(212):
-            
             b,g,r = frame_array[height][width]
             luminance = round((0.299*r) + (0.587*g) + (0.114*b))
-    if luminance > 250:
-        linha_de_pixels.append("$")
-    elif luminance > 224:
-        linha_de_pixels.append("&")
-    elif luminance > 199:
-        linha_de_pixels.append("@")
-    elif luminance > 174:
-        linha_de_pixels.append("%")
-    elif luminance > 149:
-        linha_de_pixels.append("#")
-    elif luminance > 99:
-        linha_de_pixels.append("/")
-    elif luminance > 74:
-        linha_de_pixels.append("+")
-    elif luminance > 49:
-        linha_de_pixels.append(";")
-    elif luminance > 24:
-        linha_de_pixels.append(",")
-    elif luminance >= 0:
-        linha_de_pixels.append(".")
-            
+            if luminance > 250:
+                linha_de_pixels.append("$")
+            elif luminance > 224:
+                linha_de_pixels.append("&")
+            elif luminance > 199:
+                linha_de_pixels.append("@")
+            elif luminance > 174:
+                linha_de_pixels.append("%")
+            elif luminance > 149:
+                linha_de_pixels.append("#")
+            elif luminance > 99:
+                linha_de_pixels.append("/")
+            elif luminance > 74:
+                linha_de_pixels.append("+")
+            elif luminance > 49:
+                linha_de_pixels.append(";")
+            elif luminance > 24:
+                linha_de_pixels.append(",")
+            elif luminance >= 0:
+                linha_de_pixels.append(".")
         frame_processado.append(linha_de_pixels)
     return frame_processado
 
-t1 = time.time()
+#t1 = time.time() #APENAS PARA DEBUG
 video_ascii = []
 for frame_idx in range(int(cap.get(cv.CAP_PROP_FRAME_COUNT))):
-
     ret, frame = cap.read()
     resized_frames = cv.resize(frame, (212,60)) 
     video_ascii.append(processar_pixels(resized_frames))
-t2 = time.time()
-print(f"processar_pixels {t2-t1} segundos para processar a tarefa com NUMBA NJIT")
-print("LEN:")
-print(len(video_ascii), len(video_ascii[0]),)
+#t2 = time.time() #APENAS PARA DEBUG
+#print(f"processar_pixels {t2-t1} segundos para processar a tarefa com NUMBA NJIT") #APENAS PARA DEBUG
 
 def text_frame_builder():
     rendered_frames = []
@@ -112,20 +104,19 @@ def text_frame_builder():
         frame_renderizado = ""
         for altura in range(60):
             for pixel in range(212):
-                frame_renderizado += frame_do_video_ascii[altura][pixel] #TÁ DANDO ERRO AQUI
+                frame_renderizado += frame_do_video_ascii[altura][pixel]
             frame_renderizado+="\n"
         rendered_frames.append(frame_renderizado) 
     return rendered_frames
 
-
 txt_file = open(f"{video_file_name}.txt", "w")
-t1 = time.time()
-returned_func_array = text_frame_builder()
-for frame in returned_func_array:
+returned_frame_builder = text_frame_builder() #FAZER O LOOP EM CIMA DO RETORNO DA FUNÇÃO PIORA A EFICIÊNCIA, ESTA É A MELHOR FORMA
+#t1 = time.time() #APENAS PARA DEBUG
+for frame in returned_frame_builder:
     txt_file.write(frame)
 txt_file.close()
-t2 = time.time()
-print(f"text_frame_builder levou {t2-t1} segundos para processar a tarefa com NUMBA NJIT")
+#t2 = time.time() #APENAS PARA DEBUG
+#print(f"text_frame_builder levou {t2-t1} segundos para processar a tarefa") #APENAS PARA DEBUG
 
 print("Deseja exibir o video?")
 choice = input("\n1 - Sim\n2 - Não\n")
@@ -133,9 +124,6 @@ if choice == "2":
     print("O arquivo estará renderizado para que você possa executá-lo")
     raise SystemExit
 
-for frame in returned_func_array():
+for frame in returned_frame_builder:
     print(frame)
     time.sleep(0.0333)
-
-
-#aqui vai ser diferente
